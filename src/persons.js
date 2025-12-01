a2ab8e7c6a75e6917fcbb6e2e32d1653698bc40e
Caua junq <fernandezcaua6@gmail.com>
2025-12-01 18:12:51 -0300

    chegaaa

---
+++ src/persons.js
@@ -0,0 +1,38 @@
+const persons = [
+    {
+        name: "Luiza Barreto",
+        email: "luizinhainqueita@gmail.com",
+        age: 17,
+        nickname: "lulu quentona"
+    },
+
+    {
+        name: "Mathues Murija",
+        email: "matheustigrao@gmail.com",
+        age: 16,
+        nickname: "tigrão"
+    },
+
+    {
+        name: "João Macedo",
+        email: "joaoemaria@gmail.com",
+        age: 16,
+        nickname: "pau mandado"
+    },
+
+    {
+        name: "David Costa",
+        email: "davizinhogameplay@gmail.com",
+        age: 16,
+        nickname: "pk"
+    },
+
+    {
+        name: "Maria carolina",
+        email: "mariaejoao@gmail.com",
+        age: 16,
+        nickname: "Pau que manda"
+    }
+]
+
+export {persons}
