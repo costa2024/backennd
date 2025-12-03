ea6076987b6befd809b0bbd8f68749884dd99a00
Caua junq <fernandezcaua6@gmail.com>
2025-12-02 21:28:41 -0300

    acabeiiiii

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 import express from "express"
 import cors from "cors"
 import {persons} from "./persons.js"
+import mysql from "mysql2"
 
 const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(persons)
 })
 
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, nickname, password } = request.body.user
+
+    const insertCommand = `
+    INSERT INTO cauaJunq_02ma(name, email, age, nickname, password)
+    VALUES(?, ?, ?, ?, ?)
+    `
+
+    database.query(insertCommand, [name, email, age, nickname, password], (error) => {
+        if(error){
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({message: "Usuário cadastrado com sucesso!"})
+    })
+
+    response.status(201).json({message: "Usuário cadastrado com sucesso!"})
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}!`)
+})
+
+const database = mysql.createPool({
+    host: "benserverplex.ddns.net",
+    user: "alunos",
+    password: "senhaAlunos",
+    database: "web_02ma",
+    connectionLimit: 10
 })
